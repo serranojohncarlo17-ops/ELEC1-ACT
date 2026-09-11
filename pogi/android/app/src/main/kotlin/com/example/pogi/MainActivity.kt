4e82d89f14929f7bf8e507af16e10a8a830bdbd8
serranojohncarlo17-ops <serranojohncarlo17@gmail.com>
2026-09-11 04:27:16 +0000

    Activity Updated

---
+++ pogi/android/app/src/main/kotlin/com/example/pogi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pogi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ pogi/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Sir Pogi', style: TextStyle(
+              fontSize:50,
+              color: Colors.green
+              )
+          ),
+           
+        ),
+      ),
+
+
+
+    );
+  }
+}
