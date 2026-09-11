454e6199c00372db7807c84698845e0283800617
serranojohncarlo17-ops <serranojohncarlo17@gmail.com>
2026-09-11 06:02:55 +0000

    Activity Passed

--- pogi/lib/main.dart
+++ pogi/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
@@ -18,11 +19,71 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Icon(Icons.account_circle, color: Colors.deepPurple, size: 150.0),
               Text(
-                'Sir Pogi',
-                style: TextStyle(fontSize: 50, color: Colors.green),
+                'John Carlo Serrano',
+                style: TextStyle(fontSize: 50, color: Colors.black),
               ), //text
-              Text('Hi Sir Pogi'),
+              //SizedBox(height: 10),
+              Text(
+                'Bachelor of Science in Information Technology',
+                style: TextStyle(color: Colors.grey),
+              ), //text
+
+              SizedBox(height: 200),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.home, color: Colors.deepPurple, size: 45.0),
+                SizedBox(width: 80),
+                Icon(Icons.menu_book, color: Colors.deepPurple, size: 45.0),
+                SizedBox(width: 80),
+                Icon(Icons.person_outline, color: Colors.deepPurple, size: 45.0),
+                
+              ]
+
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                
+                Text('Home'),
+                SizedBox(width: 80),
+                Text('Courses'),
+                SizedBox(width: 75),
+                Text('Profile'),
+                
+                
+              ]
+
+            ),
+
+            SizedBox(height: 150),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Ang pogi mo po Sir Rodney'),
+                
+              ]
+
+            ),
+
+            SizedBox(height: 50),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.note_alt, color: Colors.orange, size: 60.0),
+                
+                
+              ]
+
+            ),
+
+              
             ],
               
           ),//column
